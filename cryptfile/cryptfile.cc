51549518e0c827321b36d8815e41486b82627bb0
meijie <meijie@laiye.com>
2022-06-05 17:35:43 +0800

    refactor decrypt interface

--- cryptfile/cryptfile.cc
+++ cryptfile/cryptfile.cc
@@ -39,7 +39,7 @@ string DO_CBCMode_Encrypt(const string &text, byte key[], int keySize) {
     }
     catch (const CryptoPP::Exception &e) {
         cerr << e.what() << endl;
-        exit(-1);
+        exit(1);
     }
     return cipher;
 }
